1e98fc13c6c5eb4aab61317120a27dd7199be144
Georgi <georgemart0994@gmail.com>
2024-02-24 14:32:07 +0400

    Initial Commit

---
+++ app/src/main/java/com/example/easyreading/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.easyreading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTitle;
+    EditText etLogin, etPassword;
+    Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTitle = (TextView) findViewById(R.id.textView);
+        etLogin = (EditText) findViewById(R.id.editTextText3);
+        etPassword = (EditText) findViewById(R.id.editTextTextPassword);
+        btnLogin = (Button) findViewById(R.id.button);
+        btnRegister = (Button) findViewById(R.id.button2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = etLogin.getText().toString();
+                String password = etPassword.getText().toString();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
